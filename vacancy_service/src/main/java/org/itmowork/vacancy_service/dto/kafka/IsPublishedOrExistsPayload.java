1e4e9a2f157d3dd3d422ae9a7db1a32e11f4e830
kkettch <369027@niuitmo.ru>
2025-12-17 00:52:45 +0300

    feat(vacancy-service): exists() added

--- vacancy_service/src/main/java/org/itmowork/vacancy_service/dto/kafka/IsPublishedOrExistsPayload.java
+++ vacancy_service/src/main/java/org/itmowork/vacancy_service/dto/kafka/IsPublishedOrExistsPayload.java
@@ -2,4 +2,4 @@ package org.itmowork.vacancy_service.dto.kafka;
 
 import java.util.UUID;
 
-public record IsPublishedResult(UUID vacancyId, boolean published) {}
+public record IsPublishedOrExistsPayload(UUID vacancyId) {}
